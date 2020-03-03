d20a295630c6f46178cb85d4eed0ab885dbd9917
resing <hmed.bentaher@gmail.com>
2020-03-03 10:19:01 +0000

    update code

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
@@ -10,9 +11,9 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="default")
      */
-    public function index()
+    public function index(): Response
     {
-        $hello = 'test';
+        die;
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
