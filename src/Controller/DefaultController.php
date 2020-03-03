22954d4b8fed98931b11a3ee8c26bc64a558259f
resing <hmed.bentaher@gmail.com>
2020-03-03 10:24:37 +0000

    update code

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,15 +12,10 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="default")
      */
-    public function index(): Response
+    public function index(): RedirectResponse
     {
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
-    }
-
-    protected function test()
-    {
-        return 'hello word';
     }
 }
