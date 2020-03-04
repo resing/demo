e24cdc81d4384b2b6e9319ea57df3e30a756cc23
resing <hmed.bentaher@gmail.com>
2020-03-04 15:14:58 +0000

    test

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,12 +12,11 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="default")
      */
-    public function index(): bool
+    public function index(): Response
     {
+        d;
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
     }
-
-
 }
