cc64ae3f3aa140f1762aeb16bc585dccc9b88a55
resing <hmed.bentaher@gmail.com>
2020-04-13 08:24:26 +0000

    test

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,27 +15,4 @@ class DefaultController extends AbstractController
             'controller_name' => 'DefaultController',
         ]);
     }
-
-    public function testUrlCertif(): Response
-    {
-        echo 'Redirect URLs with a Trailing Slash';
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
-    }
-
-//    public function testUrlCertifWithSlah(): Response
-//    {
-//        echo 'Redirect URLs with a Trailing Slash hard Coding even without slash Symfony adding Slash';
-//        return $this->render('default/index.html.twig', [
-//            'controller_name' => 'DefaultController',
-//        ]);
-//    }
-
-    public function testUrlPattern($date)
-    {
-        echo 'good';
-
-        return new Response('yes top');
-    }
 }
